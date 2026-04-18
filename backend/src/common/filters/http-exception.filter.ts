23306ef8b08cb84072806f8a7754197c746f1643
Moonteiro <owagnerlucas0@gmail.com>
2026-04-18 09:07:22 -0300

    feat(backend): implements global error handler

---
+++ backend/src/common/filters/http-exception.filter.ts
@@ -0,0 +1,27 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    const status =
+      exception instanceof HttpException ? exception.getStatus() : 500;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : 'Internal server error';
+
+    response.status(status).send({
+      statusCode: status,
+      message,
+    });
+  }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,7 @@ import {
   FastifyAdapter,
   type NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -21,6 +22,7 @@ async function bootstrap() {
   await app.register(fastifyCookie);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   app.enableCors({
     origin: configService.get<string>('FRONTEND_URL'),
